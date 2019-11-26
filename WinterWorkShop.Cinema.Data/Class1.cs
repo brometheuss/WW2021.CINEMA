e2661d22bff747022a3fa55ad1b01191e2abcd65
Dusan Ivanisevic <d.ivanisevic@levi9.com>
2019-11-26 16:48:48 +0100

    added data and repositories project

---
+++ WinterWorkShop.Cinema.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WinterWorkShop.Cinema.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ WinterWorkShop.Cinema.Repositories/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WinterWorkShop.Cinema.Repositories
+{
+    public class Class1
+    {
+    }
+}
