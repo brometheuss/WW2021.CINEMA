5337c99ae0e778964822281b904c37bb828d9e78
Dusan Ivanisevic <d.ivanisevic@levi9.com>
2019-12-03 12:27:06 +0000

    Added 03-sql-demo.sql

---
+++ docs/03-sql-demo.sql
@@ -0,0 +1,31 @@
+-- Simple Query
+SELECT * FROM dbo.Customer;
+
+SELECT * FROM dbo.Customer
+WHERE FirstName = 'Maria';
+
+-- Group By
+SELECT COUNT(*), City FROM dbo.Customer
+GROUP BY City;
+
+-- Join
+
+SELECT O.OrderNumber, CONVERT(date,O.OrderDate) AS Date, 
+       P.ProductName, I.Quantity, I.UnitPrice 
+  FROM [Order] O 
+  JOIN OrderItem I ON O.Id = I.OrderId 
+  JOIN Product P ON P.Id = I.ProductId
+ORDER BY O.OrderNumber;
+
+-- Views
+GO
+CREATE VIEW demo
+AS 
+SELECT O.OrderNumber, O.OrderDate, I.Quantity, I.UnitPrice 
+  FROM [Order] O 
+  JOIN OrderItem I ON O.Id = I.OrderId 
+  JOIN Product P ON P.Id = I.ProductId
+
+SELECT * FROM demo
+
+-- Stored Procedures
