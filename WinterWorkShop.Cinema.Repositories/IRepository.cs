1788bd3cb90bd15917a72440d65fb05e934cb269
Dusan Ivanisevic <d.ivanisevic@levi9.com>
2019-12-17 16:41:06 +0100

    added new generic interface for reposirory

---
+++ WinterWorkShop.Cinema.Repositories/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinterWorkShop.Cinema.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        EntityEntry<T> Delete(object id);
+
+        Task<IEnumerable<T>> GetAll();
+
+        T GetById(object id);
+
+        EntityEntry<T> Insert(T obj);
+
+        void Save();
+
+        EntityEntry<T> Update(T obj);
+    }
+}
