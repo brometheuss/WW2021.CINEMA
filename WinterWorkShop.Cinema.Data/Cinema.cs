5973a8500a6d9c5fe1500546ffd63bfcc8da6c3b
Alen_Jerinic <a.jerinic@levi9.com>
2019-12-18 15:34:14 +0100

    Aded Cinema Data Model

---
+++ WinterWorkShop.Cinema.Data/Cinema.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinterWorkShop.Cinema.Data
+{
+    public class Cinema
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Auditorium> Auditoriums { get; set; }
+    }
+}
