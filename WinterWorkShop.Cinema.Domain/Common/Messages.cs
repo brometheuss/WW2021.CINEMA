51b5d717746672da0592fd30a15dc1657ae49d2c
Alen_Jerinic <a.jerinic@levi9.com>
2020-01-22 10:39:46 +0100

    Prejections region added in messages

--- WinterWorkShop.Cinema.Domain/Common/Messages.cs
+++ WinterWorkShop.Cinema.Domain/Common/Messages.cs
@@ -6,9 +6,7 @@
         public const string MOVIE_GET_ALL_AUDITORIUMS_ERROR = "Error occured while getting all auditoriums, please try again";
         #endregion
 
-        #region Movies
-        public const string PROJECTIONS_AT_SAME_TIME = "Cannot create new projection, there are projections at same time alredy.";
-        public const string PROJECTION_IN_PAST = "Projection time cannot be in past.";
+        #region Movies        
         public const string MOVIE_DOES_NOT_EXIST = "Movie does not exist.";
         public const string MOVIE_PROPERTIE_TITLE_NOT_VALID = "The movie title cannot be longer than 50 characters.";
         public const string MOVIE_PROPERTIE_YEAR_NOT_VALID = "The movie year must be between 1895-2100.";
@@ -19,6 +17,11 @@
         public const string MOVIE_GET_ALL_MOVIES_ERROR = "Error occured while getting all movies, please try again";
         #endregion
 
-        
+        #region Projections
+        public const string PROJECTIONS_AT_SAME_TIME = "Cannot create new projection, there are projections at same time alredy.";
+        public const string PROJECTION_IN_PAST = "Projection time cannot be in past.";
+        #endregion
+
+
     }
 }
