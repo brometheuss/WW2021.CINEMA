dbce62f43adb59180e3a23854e6f5170a06b94c0
Dusan Ivanisevic <d.ivanisevic@levi9.com>
2020-01-30 16:47:20 +0100

    getByUsername correction

--- WinterWorkShop.Cinema.Repositories/UsersRepository.cs
+++ WinterWorkShop.Cinema.Repositories/UsersRepository.cs
@@ -43,9 +43,11 @@ namespace WinterWorkShop.Cinema.Repositories
 
         public User GetByUserName(string username)
         {
-            var data = _cinemaContext.Users.Where(x => x.UserName == username).ToList();
+            var data = _cinemaContext.Users
+                .AsParallel()
+                .SingleOrDefault(x => x.UserName == username);
 
-            return data[0];
+            return data;
         }
 
         public User Insert(User obj)
